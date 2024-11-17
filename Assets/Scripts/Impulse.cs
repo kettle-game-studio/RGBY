30f9f5eec5433bff0c86b03650b34ea89ea9856e
RobolabGs2 <slavikelk@yandex.ru>
2024-11-18 01:35:37 +0300

    Lvl 4, lvl 5

--- Assets/Scripts/Impulse.cs
+++ Assets/Scripts/Impulse.cs
@@ -6,13 +6,14 @@ public class Impulse : MonoBehaviour
     float initialScale;
     public float bpm = 130;
     public float scaleDelta = 1;
+    public float shift = 0;
     void Start() {
         initialScale = transform.localScale.x;
     }
     // Update is called once per frame
     void Update()
     {
-        float s = initialScale - scaleDelta*Mathf.Cos(Time.time*Mathf.PI*2*(bpm/60));
+        float s = initialScale + (scaleDelta*(Mathf.Cos((shift+Time.time)*Mathf.PI*2*(bpm/60))-1));
         transform.localScale = new Vector3(s, s, s);
     }
 }
